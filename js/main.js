40a7a31b563ed3b02e10d73f0f1978f4c5b7f5fa
drmyersii <drmyersii@users.noreply.github.com>
2015-09-03 00:35:38 -0400

    Adding basic connection test.
    Two peers are successfully connecting and sharing data.

--- js/main.js
+++ js/main.js
@@ -8,6 +8,26 @@
 
     peer.on('open', function (id)
     {
-        document.getElementById(SHARE_URL_ID).value = window.location.href;
+        document.getElementById(SHARE_URL_ID).value = window.location.href + '#id=' + id;
+    });
+
+    var ORIGIN_ID = window.location.hash.split('=')[1] || '';
+
+    if (ORIGIN_ID !== '')
+    {
+        var conn = peer.connect(ORIGIN_ID);
+
+        conn.on('open', function ()
+        {
+            conn.send('testing!');
+        });
+    }
+
+    peer.on('connection', function (conn)
+    {
+        conn.on('data', function (data)
+        {
+            console.log(data);
+        });
     });
 })(window, document);
