c42c6212eb63649d2ca2baaa287a2f7b4bbab57f
drmyersii <drmyersii@users.noreply.github.com>
2015-09-03 00:15:01 -0400

    Adding initialization of PeerJS conneciton.
    Also moved PeerJS API key to its own constant. Please use your own API key if you are going to launch your own version of this application. Thanks! :D

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 ;(function (window, document, undefined)
 { 'use strict';
 
-    var peer = new Peer({ key: 'w19j6rp47wx9a4i' });
+    var SHARE_URL_ID = 'share-url';
+    var PEERJS_API_KEY = 'w19j6rp47wx9a4i';
+
+    var peer = new Peer({ key: PEERJS_API_KEY });
+
+    peer.on('open', function (id)
+    {
+        document.getElementById(SHARE_URL_ID).value = window.location.href;
+    });
 })(window, document);
