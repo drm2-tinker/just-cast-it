ad8dacd622dca0e09d605889262836cb773ff6e3
drmyersii <drmyersii@users.noreply.github.com>
2015-09-05 15:01:03 -0400

    Adding message bus listener to receiver.

--- receiver/js/main.js
+++ receiver/js/main.js
@@ -1,4 +1,5 @@
 // window.mediaElement = document.getElementById('media');
 // window.mediaManager = new cast.receiver.MediaManager(window.mediaElement);
 window.castReceiverManager = cast.receiver.CastReceiverManager.getInstance();
+window.castReceiverManager.getCastMessageBus('urn:x-cast:io.renobit.apps.just-cast-it');
 window.castReceiverManager.start();
