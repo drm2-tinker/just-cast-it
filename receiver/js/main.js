83811650120789ddddca16b34d393a5becca89c8
drmyersii <drmyersii@users.noreply.github.com>
2015-09-07 02:41:02 -0400

    Refactoring code for receiver app...

--- receiver/js/main.js
+++ receiver/js/main.js
@@ -1,5 +1,42 @@
-// window.mediaElement = document.getElementById('media');
-// window.mediaManager = new cast.receiver.MediaManager(window.mediaElement);
-window.castReceiverManager = cast.receiver.CastReceiverManager.getInstance();
-window.castMessageBus = window.castReceiverManager.getCastMessageBus('urn:x-cast:io.renobit.apps.just-cast-it');
-window.castReceiverManager.start();
+;window.Caster = (function (window, document, undefined)
+{ 'use strict';
+
+    // the private property/method store
+    var _p = {};
+
+    // the main application constructor
+    var Caster = function ()
+    {
+        // private local properties (constructor access only)
+        var THAT = this;
+
+        // private properties
+        _p.cast   = {
+            message_bus:      null,
+            receiver_manager: null,
+            sender_list:      null,
+            session:          null
+        };
+        _p.peerjs = {
+            api_key:          'w19j6rp47wx9a4i',
+            connection:       null,
+            id:               null,
+            peer:             null,
+            sender_id:        null
+        };
+
+        // private methods
+        _p.initializeCastAPI = function ()
+        {
+            _p.cast.receiver_manager = cast.receiver.CastReceiverManager.getInstance();
+            _p.cast.message_bus      = _p.cast.receiver_manager.getCastMessageBus('urn:x-cast:io.renobit.apps.just-cast-it');
+
+            _p.cast.receiver_manager.start();
+        };
+    };
+
+    Caster.prototype.Start = function ()
+    {
+        _p.initializeCastAPI();
+    };
+})(window, document);
